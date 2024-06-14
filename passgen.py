1c0468c26bfb84151fdd8a74c4a07ebc3972cda8
m333ga <alekseichernykh1@gmail.com>
2024-06-14 23:15:03 +0500

    Fri Jun 14 23:15:03 +05 2024

--- passgen.py
+++ passgen.py
@@ -3,16 +3,19 @@ from random import randint
 from rich.console import Console
 import passgencfg
 
-#
+# print intro
 console = Console()
-console.print('~ passgen ~', style='bold red')
+console.print('~ PASSGEN ~', style='bold red')
 
-password_length = int(input('password length: '))
+try:
+    password_length = int(input('password length: '))
+except ValueError:
+    print('error. password length must be an integer.')
 
 password_symbols_pull = str()
 
-if passgencfg.include_digits:
-    password_symbols_pull += passgencfg.digits
+if passgencfg.include_numbers:
+    password_symbols_pull += passgencfg.numbers
 if passgencfg.include_letters:
     password_symbols_pull += passgencfg.letters
 if passgencfg.include_capital_letters:
@@ -21,9 +24,15 @@ if passgencfg.include_special_chars:
     password_symbols_pull += passgencfg.special_chars
 
 password = str()
-# for symbol in password_symbols_pull:
-#     password = password_symbols_pull[randint(0, len(password_symbols_pull) - 1)]
 
-print(password_symbols_pull.split(''))
+try:
+    for i in range(password_length):
+        symbol = password_symbols_pull[randint(
+            0, len(password_symbols_pull)-1)]
+        password += symbol
+except ValueError:
+    print('error. no symbols selected in passgencfg.py')
+except:
+    print('error.')
 
 print(password)
--- passgencfg.py
+++ passgencfg.py
@@ -1,10 +1,9 @@
-digits = '0 1 2 3 4 5 6789'
+numbers = '0123456789'
 letters = 'abcdefghijklmnopqrstuvwxyz'
 capital_letters = letters.upper()
 special_chars = '!@#$%^&*()_+-=[{]};:",.<>/?'
 
-include_digits = True
+include_numbers = True
 include_letters = True
 include_capital_letters = True
 include_special_chars = True
-include_spaces = True
