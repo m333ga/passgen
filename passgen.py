ff58cd2a2ed655714033ada8da8847bba68de2ef
m333ga <alekseichernykh1@gmail.com>
2024-06-14 21:29:24 +0500

    Fri Jun 14 21:29:24 +05 2024

--- passgen.py
+++ passgen.py
@@ -1,4 +1,29 @@
-# from colorama import Fore
+# import libs
+from random import randint
 from rich.console import Console
+import passgencfg
+
+#
 console = Console()
 console.print('~ passgen ~', style='bold red')
+
+password_length = int(input('password length: '))
+
+password_symbols_pull = str()
+
+if passgencfg.include_digits:
+    password_symbols_pull += passgencfg.digits
+if passgencfg.include_letters:
+    password_symbols_pull += passgencfg.letters
+if passgencfg.include_capital_letters:
+    password_symbols_pull += passgencfg.capital_letters
+if passgencfg.include_special_chars:
+    password_symbols_pull += passgencfg.special_chars
+
+password = str()
+# for symbol in password_symbols_pull:
+#     password = password_symbols_pull[randint(0, len(password_symbols_pull) - 1)]
+
+print(password_symbols_pull.split(''))
+
+print(password)
---
+++ passgencfg.py
@@ -0,0 +1,10 @@
+digits = '0 1 2 3 4 5 6789'
+letters = 'abcdefghijklmnopqrstuvwxyz'
+capital_letters = letters.upper()
+special_chars = '!@#$%^&*()_+-=[{]};:",.<>/?'
+
+include_digits = True
+include_letters = True
+include_capital_letters = True
+include_special_chars = True
+include_spaces = True
